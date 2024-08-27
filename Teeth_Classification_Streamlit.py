e770ae29c6b0d02c29e225aaa731a8968c22dd78
OmarOssama25 <160012480+OmarOssama25@users.noreply.github.com>
2024-08-27 16:40:17 +0300

    Update Teeth_Classification_Streamlit.py

--- Teeth_Classification_Streamlit.py
+++ Teeth_Classification_Streamlit.py
@@ -8,7 +8,7 @@ model = tf.keras.models.load_model('teeth_classification_model.h5')
 
 # Preprocess function
 def preprocess_image(image):
-    image = image.resize((100, 100))
+    image = image.resize((150, 150))
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=0)
     return image
